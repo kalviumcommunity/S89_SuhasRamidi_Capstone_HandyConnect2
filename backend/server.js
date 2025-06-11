1a719c67fece1e5d64bcd67f2a87c8ce027099ea
hime2828 <ramidisaisuhas@gmail.com>
2025-06-11 11:43:34 +0530

    deploy

--- backend/server.js
+++ backend/server.js
@@ -10,13 +10,14 @@ app.use(express.json());
 // Use booking routes
 app.use('/api', bookingRoutes);
 
-app.get("/bookings", (req, res) => {
+app.get("/", (req, res) => {
     res.send("HandyConnect backend is running!");
 });
+ const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URL)
   .then(() => {
     console.log('Connected to MongoDB');
-    const PORT = process.env.PORT || 5000;
+   
     
     app.listen(PORT, () => {
       console.log(`Server is running on http://localhost:${PORT}`);
